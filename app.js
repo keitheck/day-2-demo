efc8f9bf0f5de63ca547a5d62b8f15e9a00a1436
keitheck <eckertkeith@gmail.com>
2017-12-05 12:47:48 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var kids = prompt('how many kids does brian have?');
+console.log('number of kids', kids);
+
+var hats = prompt('how many hats does brian have?');
+console.log('number of hats', hats);
+
+//toLowerCase
+//toUpperCase
+//parseInt
+
+// || is an 'OR' operator
+// && is an 'AND' operator
+
+if (kids === '3' || kids === '4') {
+  alert('u right');
+
+}
+
+if (kids === '3' && hats.toLowercase() === 'none') {
+  alert('u right again.');
+
+}
